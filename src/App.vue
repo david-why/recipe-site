973e2e24bb3bb386b35ca2880dde6f5c863723d9
david-why <david_why@outlook.com>
2025-09-10 10:18:11 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">
+        <img
+          class="d-inline-block align-text-top"
+          width="24"
+          height="24"
+          src="@/assets/icon256.png"
+        />
+        {{ ' ' }}
+        <span class="gradient-text">Sustenance</span>
+      </RouterLink>
+    </div>
+  </nav>
+  <div class="container">
+    <h1>You did it!</h1>
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
