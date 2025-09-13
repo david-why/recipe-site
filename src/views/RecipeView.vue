b6dc5c720d12549cceefa9af39197114f906b6be
david-why <david_why@outlook.com>
2025-09-13 19:43:28 +0800

    add carousel for recipe images

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import LoadingIcon from '@/components/LoadingIcon.vue'
 import DurationText from '@/components/text/DurationText.vue'
-import RecipeDurationsText from '@/components/text/RecipeDurationsText.vue'
 import { getRecipeById } from '@/service'
-import { onMounted, ref } from 'vue'
+import { Carousel } from 'bootstrap'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import type { Recipe } from '~/shared/types'
 const route = useRoute()
@@ -11,6 +11,9 @@ const route = useRoute()
 const recipeId = route.params.recipeId as string
 
 // state
+
+const carouselEl = ref<HTMLElement>()
+const carousel = ref<Carousel>()
 
 const recipe = ref<Recipe | null>(null)
 
@@ -24,6 +27,15 @@ const updateRecipe = async () => {
 
 onMounted(async () => {
   updateRecipe()
+  if (carouselEl.value) {
+    carousel.value = new Carousel(carouselEl.value)
+  }
+})
+
+onBeforeUnmount(() => {
+  if (carousel.value) {
+    carousel.value.dispose()
+  }
 })
 </script>
 
@@ -33,12 +45,53 @@ onMounted(async () => {
   </div>
   <div v-else>
     <div class="row">
-      <div class="col-12 col-lg-6">
-        <img class="img-fluid rounded" :src="recipe.images[0]" />
+      <div class="col-12 col-lg-5">
+        <div class="carousel slide" id="recipeCarousel" ref="carouselEl">
+          <div class="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#recipeCarousel"
+              :data-bs-slide-to="index"
+              :class="{ active: index === 0 }"
+              aria-current="true"
+              :aria-label="`Image ${index + 1}`"
+              v-for="(url, index) in recipe.images"
+              :key="url"
+            ></button>
+          </div>
+          <div class="carousel-inner">
+            <div
+              class="carousel-item"
+              :class="{ active: index === 0 }"
+              v-for="(url, index) in recipe.images"
+              :key="url"
+            >
+              <img :src="url" class="d-block w-100" :alt="`Image ${index + 1}`" />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#recipeCarousel"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#recipeCarousel"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
-      <div class="col-12 col-lg-6">
+      <div class="col-12 col-lg-7 my-auto">
         <h1>{{ recipe.title }}</h1>
-        <ul class="list-unstyled d-flex flex-column gap-2 mt-4">
+        <ul class="list-unstyled d-flex flex-column gap-3 mt-4">
           <li v-if="recipe.active_time">
             <i class="bi bi-fork-knife"></i> Preparation time:
             <DurationText :time="recipe.active_time" />
@@ -54,10 +107,10 @@ onMounted(async () => {
       </div>
     </div>
 
-    <!-- <textarea
+    <textarea
       class="form-control"
       style="height: 20rem"
       :value="JSON.stringify(recipe, null, 4)"
-    ></textarea> -->
+    ></textarea>
   </div>
 </template>
