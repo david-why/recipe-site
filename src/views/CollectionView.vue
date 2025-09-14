fe336b4bcb2350b915e67864007d9fbac79c82da
david-why <david_why@outlook.com>
2025-09-14 15:45:27 +0800

    give up on old ios :(

--- src/views/CollectionView.vue
+++ src/views/CollectionView.vue
@@ -11,42 +11,20 @@ const collectionId = route.params.collectionId as string
 
 // state
 
-const collectionLoading = ref(true)
-const collection = ref<Collection>({ id: '', title: '', description: '', image: '' })
+const collection = ref<Collection | null>(null)
 const recipesLoading = ref(false)
-const recipes = ref<{ recipes: Recipe[] }>({
-  recipes: [
-    {
-      id: '',
-      title: '',
-      tags: [],
-      active_time: 0,
-      total_time: 0,
-      difficulty: 'easy',
-      serving_size: 0,
-      serving_unit: '',
-      images: [],
-      additional_info: [],
-      created_at: '',
-    },
-  ],
-})
+const recipes = ref<Recipe[]>([])
 
 // functions
 
 async function updateCollection() {
-  collectionLoading.value = true
-  try {
-    collection.value = await getCollectionById(collectionId)
-  } finally {
-    collectionLoading.value = false
-  }
+  collection.value = await getCollectionById(collectionId)
 }
 
 async function updateRecipes() {
   recipesLoading.value = true
   try {
-    recipes.value.recipes = await getCollectionRecipes(collectionId)
+    recipes.value = await getCollectionRecipes(collectionId)
   } finally {
     recipesLoading.value = false
   }
@@ -72,11 +50,11 @@ onMounted(() => {
     </div>
     <div v-else>
       <div class="row g-3 mt-4 align-items-stretch">
-        <div class="col-6 col-md-4 col-lg-3" v-for="recipe in recipes.recipes" :key="recipe.id">
+        <div class="col-6 col-md-4 col-lg-3" v-for="recipe in recipes" :key="recipe.id">
           <RecipeCard class="h-100" :recipe="recipe" />
         </div>
       </div>
-      <div v-if="recipes.recipes.length === 0">
+      <div v-if="recipes.length === 0">
         <p>No recipes in this collection.</p>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    legacy({
-      targets: 'ios_saf >= 9, dead',
-      renderLegacyChunks: true,
-      modernPolyfills: true,
-      externalSystemJS: true,
-    }),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
